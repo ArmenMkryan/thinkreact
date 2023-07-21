446f90663af0efd2a712243855e94be1ddca79e9
Armen Mkryan <armenmkryan@gmail.com>
2023-07-21 17:33:26 +0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,86 @@
-import { useState } from "react";
+import React from 'react';
+import { getImageUrl } from './utils.js';
+import "./style.css";
 
-function FilterableProductTable({ products }) {
-  const [filterText, setFilterText] = useState("");
-  const [inStockOnly, setInStockOnly] = useState(false);
+
+function Bulki ({name, isBald}) {
 
   return (
-    <div>
-      <SearchBar
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-        onFilterTextChange={setFilterText}
-        onInStockOnlyChange={setInStockOnly}
+    <li className="item">
+      {isBald ? name + ' is Bald' : name}
+    </li>
+  );
+}
+
+function Profile({ name, imageUrl, profession, awards, discovery }) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageUrl)}
+        alt={name}
+        width={70}
+        height={70}
       />
-      <ProductTable
-        products={products}
-        filterText={filterText}
-        inStockOnly={inStockOnly}
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: </b>
+          {Array.isArray(awards) && awards.length > 0 ? (
+            <span>{awards.join(', ')}</span>
+          ) : (
+            <span>No awards</span>
+          )}
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovery}
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default function Gallery() {
+  return (
+    <div>
+      <Bulki 
+        isBald={true}
+        name="1"/>
+      <Bulki 
+        isBald={false}
+        name="2"
+        />
+      <Bulki 
+        isBald={true}
+        name="3"/>
+        <Bulki 
+        isBald={false}
+        name="4"/>
+      <h1>Notable Scientists</h1>
+      <Profile
+        name="Maria Skłodowska-Curie"
+        imageUrl="szV5sdG"
+        profession="physicist and chemist"
+        awards={[
+          'Nobel Prize in Physics',
+          'Nobel Prize in Chemistry',
+          'Davy Medal',
+          'Matteucci Medal',
+        ]}
+        discovery="polonium (element)"
+      />
+      <Profile
+        name="Katsuko Saruhashi"
+        imageUrl="YfeOqp2"
+        profession="geochemist"
+        awards={['Miyake Prize for geochemistry', 'Tanaka Prize']}
+        discovery="a method for measuring carbon dioxide in seawater"
       />
     </div>
   );
-}
-
-function ProductCategoryRow({ category }) {
-  return (
-    <tr>
-      <th colSpan="2">{category}</th>
-    </tr>
-  );
-}
-
-function ProductRow({ product }) {
-  const name = product.stocked ? (
-    product.name
-  ) : (
-    <span style={{ color: "red" }}>{product.name}</span>
-  );
-
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
-    </tr>
-  );
-}
-
-function ProductTable({ products, filterText, inStockOnly }) {
-  const rows = [];
-  let lastCategory = null;
-
-  products.forEach((product) => {
-    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
-      return;
-    }
-    if (inStockOnly && !product.stocked) {
-      return;
-    }
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category}
-        />
-      );
-    }
-    rows.push(<ProductRow product={product} key={product.name} />);
-    lastCategory = product.category;
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-function SearchBar({
-  filterText,
-  inStockOnly,
-  onFilterTextChange,
-  onInStockOnlyChange,
-}) {
-  return (
-    <form>
-      <input
-        type="text"
-        value={filterText}
-        placeholder="Search..."
-        onChange={(e) => onFilterTextChange(e.target.value)}
-      />
-      <label>
-        <input
-          type="checkbox"
-          checked={inStockOnly}
-          onChange={(e) => onInStockOnlyChange(e.target.checked)}
-        />{" "}
-        Only show products in stock
-      </label>
-    </form>
-  );
-}
-
-const PRODUCTS = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-];
-
-export default function App() {
-  return <FilterableProductTable products={PRODUCTS} />;
 }
